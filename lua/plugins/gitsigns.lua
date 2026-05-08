dd4a3165e4e9edb3f8aa7de15eaf8eb35af22e23
spidychoipro <79796502+spidychoipro@users.noreply.github.com>
2026-05-08 19:21:26 +0900

    feat: add gitsigns workflow integration

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,61 @@
+return {
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup({
+                signs = {
+                    add = { text = "┃" },
+                    change = { text = "┃" },
+                    delete = { text = "_" },
+                    topdelete = { text = "‾" },
+                    changedelete = { text = "~" },
+                    untracked = { text = "┆" },
+                },
+                on_attach = function(bufnr)
+                    local gs = package.loaded.gitsigns
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    -- Navigation
+                    map("n", "]c", function()
+                        if vim.wo.diff then
+                            return "]c"
+                        end
+                        vim.schedule(function()
+                            gs.next_hunk()
+                        end)
+                        return "<Ignore>"
+                    end, { expr = true, desc = "Next hunk" })
+
+                    map("n", "[c", function()
+                        if vim.wo.diff then
+                            return "[c"
+                        end
+                        vim.schedule(function()
+                            gs.prev_hunk()
+                        end)
+                        return "<Ignore>"
+                    end, { expr = true, desc = "Prev hunk" })
+
+                    -- Actions
+                    map("n", "<leader>gs", gs.stage_hunk, { desc = "Stage hunk" })
+                    map("n", "<leader>gr", gs.reset_hunk, { desc = "Reset hunk" })
+                    map("v", "<leader>gs", function()
+                        gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                    end, { desc = "Stage hunk" })
+                    map("v", "<leader>gr", function()
+                        gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                    end, { desc = "Reset hunk" })
+                    map("n", "<leader>gp", gs.preview_hunk, { desc = "Preview hunk" })
+                    map("n", "<leader>gb", function()
+                        gs.blame_line({ full = true })
+                    end, { desc = "Blame line" })
+                end,
+            })
+        end,
+    },
+}
