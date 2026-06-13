9c2b0088974eedaea671d7c9f4d5da3d0aff512f
spidychoipro <spidychoipro@users.noreply.github.com>
2026-06-14 01:57:19 +0900

    LSP 설정 및 toggleterm 플러그인 추가

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,51 +1,57 @@
 return {
     {
-    "mason-org/mason.nvim",
-    config = function()
-	require("mason").setup()
-    end,
+        "mason-org/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
     },
     {
-	"mason-org/mason-lspconfig.nvim",
-	config = function()
-	require("mason-lspconfig").setup({
-	    ensure_installed = { "lua_ls", "basedpyright", "bashls"}
-	    })
-	end
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "basedpyright", "bashls" }
+            })
+        end
     },
     {
-	"neovim/nvim-lspconfig",
-	config = function()
-	    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            
+            -- Lua LSP 설정
+            vim.lsp.config.lua_ls = {
+                cmd = { "lua-language-server" },
+                root_markers = { ".luarc.json", ".git" },
+                filetypes = { "lua" },
+                capabilities = capabilities
+            } 
 
-	    vim.lsp.config.lua_ls = {
-		cmd = { "lua-language-server" },
-		root_markers = { ".luarc.json", ".git" },
-		filetypes = { "lua" },
-		capabilities = capabilities
-	    }
-
-	    vim.lsp.config.basedpyright = {
-                cmd = {"/usr/local/bin/basedpyright-langserver", "--stdio"},
+            -- Python LSP (basedpyright) 설정
+            -- 💡 에러의 원인이었던 'root_dir' 함수를 제거했습니다.
+            vim.lsp.config.basedpyright = {
+                cmd = {"basedpyright-langserver", "--stdio"}, 
                 root_markers = { "pyproject.toml", "setup.py", ".git" },
                 filetypes = { "python" },
-		capabilities = capabilities
+                capabilities = capabilities,
             }
 
-	    vim.lsp.config.bashls = {
-		cmd = { "bash-language-server", "start" },
-		filetypes = { "sh", "bash" },
-		root_markers = { ".git", "ShellCheckrc" },
-		capabilities = capabilities
-	    }
+            -- Bash LSP 설정
+            vim.lsp.config.bashls = {
+                cmd = { "bash-language-server", "start" },
+                filetypes = { "sh", "bash" },
+                root_markers = { ".git", "ShellCheckrc" },
+                capabilities = capabilities
+            }
 
-	    vim.lsp.enable("lua_ls")
-	    vim.lsp.enable("basedpyright")
-	    vim.lsp.enable("bashls")
+            -- LSP 활성화 (사용자 방식 유지)
+            vim.lsp.enable("lua_ls")
+            vim.lsp.enable("basedpyright")
+            vim.lsp.enable("bashls")
 
-	    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-	    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-	    vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-	end
+            -- 키매핑 (사용자 방식 유지)
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+        end
     }
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,9 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.g.mapleader =  " "
 vim.opt.clipboard = "unnamedplus"
+
+vim.keymap.set("n", "<C-S-v>", '"+p')
+vim.keymap.set("i", "<C-S-v>", function()
+  local text = vim.fn.getreg('+')
+  vim.api.nvim_put({text}, 'c', true, true)
+end)
