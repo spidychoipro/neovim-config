3c791a993e7e0baaf0af988d3914c04cc7022c40
spidychoipro <spidychoipro@users.noreply.github.com>
2026-06-14 01:57:19 +0900

    chore: optimize and stabilize config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,25 +2,36 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
     config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local trouble = require("trouble.sources.telescope")
+
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = { ["<c-t>"] = trouble.open },
+            n = { ["<c-t>"] = trouble.open },
+          },
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+
+      telescope.load_extension("ui-select")
+
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = "Find files" })
-      vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = "Live grep" })
+      vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help" })
     end,
   },
-    {
-	"nvim-telescope/telescope-ui-select.nvim",
-	config = function()
-	require("telescope").setup ({
-	    extensions = {
-		["ui-select"] = {
-		    require("telescope.themes").get_dropdown {
-		    }
-		}
-	    }
-	})
-	    require("telescope").load_extension("ui-select")
-	end
-    },
 }
