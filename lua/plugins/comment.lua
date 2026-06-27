beb5dd165bfcc7aa0e1e27cd37a0897e02572a73
spidychoipro <spidychoipro@users.noreply.github.com>
2026-06-27 19:02:48 +0900

    fix: pre_hook returns '''%s''' for blockwise comment on Python

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,11 @@
 return {
     "numToStr/Comment.nvim",
     opts = {
-        pre_hook = function()
+        pre_hook = function(ctx)
+            local U = require("Comment.utils")
+            if ctx.ctype == U.ctype.blockwise then
+                return "'''%s'''"
+            end
             return vim.bo.commentstring
         end,
         padding = true,
