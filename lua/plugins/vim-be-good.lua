c64563d4285b3e772267d4ee03876d9e30e5a4c2
spidychoipro <spidychoipro@users.noreply.github.com>
2026-07-09 16:06:49 +0900

    fix: set VimBeGood padding to 0 so floating window covers entire editor

--- lua/plugins/vim-be-good.lua
+++ lua/plugins/vim-be-good.lua
@@ -2,15 +2,20 @@ return {
     {
         "ThePrimeagen/vim-be-good",
         cmd = "VimBeGood",
+        init = function()
+            vim.g.vim_be_good_window_padding_row = 0
+            vim.g.vim_be_good_window_padding_col = 0
+        end,
         keys = {
             {
                 "<leader>vg",
                 function()
                     pcall(vim.cmd, "Neotree close")
                     vim.cmd("tabnew")
+                    vim.cmd("only")
                     vim.cmd("VimBeGood")
                 end,
-                desc = "VimBeGood (isolated tab)",
+                desc = "VimBeGood",
             },
         },
     },
