78463b74f245216476b1276cfce5df9e72edc594
spidychoipro <spidychoipro@users.noreply.github.com>
2026-07-09 16:25:19 +0900

    fix: patch VimBeGood Buffer to remove empty first line

--- lua/plugins/vim-be-good.lua
+++ lua/plugins/vim-be-good.lua
@@ -6,6 +6,48 @@ return {
             vim.g.vim_be_good_window_padding_row = 0
             vim.g.vim_be_good_window_padding_col = 0
         end,
+        config = function()
+            local function empty(n)
+                local lines = {}
+                for i = 1, n do
+                    lines[i] = ""
+                end
+                return lines
+            end
+
+            local Buffer = require("vim-be-good.buffer")
+            Buffer.clear = function(self)
+                local len = #(self.lastRenderedInstruction or {}) + #(self.lastRendered or {})
+                vim.api.nvim_buf_set_lines(self.bufh, 0, len, false, empty(len))
+            end
+            Buffer.render = function(self, lines)
+                self.instructions = self.instructions or {}
+                self.lastRendered = self.lastRendered or {}
+                self.lastRenderedInstruction = self.lastRenderedInstruction or {}
+                self:clear()
+                self.lastRendered = lines
+                local offset = 0
+                if self.debugLineStr then
+                    vim.api.nvim_buf_set_lines(self.bufh, 0, 1, false, { self.debugLineStr })
+                    offset = 1
+                end
+                if #self.instructions > 0 then
+                    vim.api.nvim_buf_set_lines(self.bufh, offset, offset + #self.instructions, false, self.instructions)
+                    offset = offset + #self.instructions
+                end
+                vim.api.nvim_buf_set_lines(self.bufh, offset, offset + #lines, false, lines)
+            end
+            Buffer.getGameLines = function(self)
+                local startOffset = #(self.instructions or {})
+                local len = #(self.lastRendered or {})
+                return vim.api.nvim_buf_get_lines(self.bufh, startOffset, startOffset + len, false)
+            end
+            Buffer.clearGameLines = function(self)
+                local startOffset = #(self.instructions or {})
+                local len = #(self.lastRendered or {})
+                vim.api.nvim_buf_set_lines(self.bufh, startOffset, startOffset + len, false, empty(len))
+            end
+        end,
         keys = {
             {
                 "<leader>vg",
